5ea490f3850fc69022f220ca05693c4938d105dd
Mohammad Ali <mohammad.ali@hsw-stud.de>
2023-09-14 15:18:49 +0200

    wip

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+export default function Navbar() {
+    return (
+        <main>
+        
+        <header>
+          <div className='relative'>
+            <div className='bg-black h-8' style={{ width:'100%', height:'64px'}}></div>
+              <div style={{background: 'linear-gradient(135deg,black 0%,black 33%,white 33%,white 66%,#019d3a 66%,#019d3a 100%)', width:'100%', height:'44px'}}>
+                <div>
+                  <img src='/Users/mscorp14/Projects/96 - Website/hannover96-virtuelles-museum-prototyp/src/H96_Header_Logo.png' alt='Hallo'></img>
+
+                  
+                </div>
+              </div>
+            </div>
+        </header>
+      </main>
+
+
+            
+    )
+}
