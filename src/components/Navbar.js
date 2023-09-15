9262887c3f160fc9b0f6ad75e1fe0639b27aa4d4
Hosaan01 <Hosaan01jaf@gmail.com>
2023-09-15 11:58:55 +0200

    added brand logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Navbar() {
     return (
         <main>
@@ -6,17 +8,14 @@ export default function Navbar() {
           <div className='relative'>
             <div className='bg-black h-8' style={{ width:'100%', height:'64px'}}></div>
               <div style={{background: 'linear-gradient(135deg,black 0%,black 33%,white 33%,white 66%,#019d3a 66%,#019d3a 100%)', width:'100%', height:'44px'}}>
-                <div>
-                  <img src='/Users/mscorp14/Projects/96 - Website/hannover96-virtuelles-museum-prototyp/src/H96_Header_Logo.png' alt='Hallo'></img>
-
-                  
+                <div className="navbarBrand">
+                  <a className="navbarIcon">
+                    <Image src="/H96_Header_Logo.png" alt="Logo" width={110} height={96}></Image>
+                  </a>
                 </div>
               </div>
             </div>
         </header>
-      </main>
-
-
-            
+      </main>            
     )
 }
