cbc0b2509c7b3ef0440d729f1a12854a1748c21d
Mohammad Ali <mohammad.ali@hsw-stud.de>
2023-09-20 09:41:03 +0200

    Header+placeholder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function Navbar() {
     return (
         <main>
@@ -5,11 +7,16 @@ export default function Navbar() {
             <div className='relative bg-white'>
               <div className='bg-black h-8' style={{ width: '100%', height: '64px' }}></div>
               <div style={{ background: 'linear-gradient(135deg,black 0%,black 33%,white 33%,white 66%,#019d3a 66%,#019d3a 100%)', width: '100%', height: '44px' }}>
+              <div className="navbarBrand">
+                  <a className="navbarIcon">
+                    <Image src="/H96_Header_Logo.png" alt="Logo" width={110} height={96}></Image>
+                  </a>
+                </div>
                 <div className='flex justify-between items-center h-full px-4'>
                   <div className="flex items-center">
-                    <img src='/Users/mscorp14/Projects/96 - Website/hannover96-virtuelles-museum-prototyp/src/H96_Header_Logo.png' alt='Hallo' className="h-8 w-8 rounded-full" />
+                    
                   </div>
-                  <div className="bg-white rounded-lg p-4 mt-[-65px]">
+                  <div className="bg-white rounded-lg p-4 mt-[-150px]">
                     <div className="flex items-center">
                       <div className="w-12 h-12 bg-gray-300 rounded-full mr-2"></div> {/* Platzhalter für Profilbild */}
                       <div className="flex flex-col">
