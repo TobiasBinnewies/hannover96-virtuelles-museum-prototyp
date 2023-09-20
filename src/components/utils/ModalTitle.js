e60f8ce5d91bda8008e15d48843e005be1effc43
Mohammad Ali <mohammad.ali@hsw-stud.de>
2023-09-20 09:31:11 +0200

    Revert "Merge remote-tracking branch 'origin/section' into create-navbar"
    This reverts commit fd3a4be557f333983bde9ba00cf7e6a6f9ef8259, reversing
    changes made to 1341df6bfe569176969e212fd68df348f2be70d7.

--- src/components/utils/ModalTitle.js
+++
@@ -1,7 +0,0 @@
-export default function ModalTitle(props){
-  return (
-    <div>
-      <h2 className={'text-emerald-500 text-3xl font-sans font-extrabold mb-2'}>{props.title}</h2>
-    </div>
-  )
-}
