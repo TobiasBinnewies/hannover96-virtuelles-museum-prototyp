1341df6bfe569176969e212fd68df348f2be70d7
Mohammad Ali <mohammad.ali@hsw-stud.de>
2023-09-20 09:30:24 +0200

    Logout Button + placeholder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 export default function Navbar() {
     return (
         <main>
-        
-        <header>
-          <div className='relative'>
-            <div className='bg-black h-8' style={{ width:'100%', height:'64px'}}></div>
-              <div style={{background: 'linear-gradient(135deg,black 0%,black 33%,white 33%,white 66%,#019d3a 66%,#019d3a 100%)', width:'100%', height:'44px'}}>
-                <div>
-                  <img src='/Users/mscorp14/Projects/96 - Website/hannover96-virtuelles-museum-prototyp/src/H96_Header_Logo.png' alt='Hallo'></img>
-
-                  
+          <header>
+            <div className='relative bg-white'>
+              <div className='bg-black h-8' style={{ width: '100%', height: '64px' }}></div>
+              <div style={{ background: 'linear-gradient(135deg,black 0%,black 33%,white 33%,white 66%,#019d3a 66%,#019d3a 100%)', width: '100%', height: '44px' }}>
+                <div className='flex justify-between items-center h-full px-4'>
+                  <div className="flex items-center">
+                    <img src='/Users/mscorp14/Projects/96 - Website/hannover96-virtuelles-museum-prototyp/src/H96_Header_Logo.png' alt='Hallo' className="h-8 w-8 rounded-full" />
+                  </div>
+                  <div className="bg-white rounded-lg p-4 mt-[-65px]">
+                    <div className="flex items-center">
+                      <div className="w-12 h-12 bg-gray-300 rounded-full mr-2"></div> {/* Platzhalter für Profilbild */}
+                      <div className="flex flex-col">
+                        <div className="text-black font-semibold text-sm">Max Mustermann</div>
+                        <button className="bg-cyan-600 text-white py-1 px-2 rounded mt-1">Logout</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-        </header>
-      </main>
-
-
-            
+          </header>
+      </main>       
     )
 }
