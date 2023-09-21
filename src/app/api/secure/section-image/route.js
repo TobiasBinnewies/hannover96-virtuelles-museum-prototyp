72860b6e9e189e095a0fb1a8f088fe08a56b1522
TobiasMBP <binnewiestobias@gmail.com>
2023-09-21 14:42:31 +0200

    Fixed path bug 2

--- src/app/api/secure/section-image/route.js
+++ src/app/api/secure/section-image/route.js
@@ -1,6 +1,7 @@
 import apiHandler from '@backend/api/api-helper'
 import { writeFile } from 'fs/promises'
 import { NextResponse } from 'next/server'
+import path from 'path'
 
 export async function POST(req) {
   return apiHandler(handler)(req)
@@ -17,14 +18,16 @@ async function handler(req) {
     return NextResponse.json({ success: false })
   }
 
+  // console.log("cdw", path.join(process.cwd(), 'public', file.name));
+
   const bytes = await file.arrayBuffer()
   const buffer = Buffer.from(bytes)
 
   // With the file data in the buffer, you can do whatever you want with it.
   // For this, we'll just write it to the filesystem in a new location
-  const path = `public/${file.name}`
-  await writeFile(path, buffer)
-  console.log(`open ${path} to see the uploaded file`)
+  const pathname = path.join(process.cwd(), 'public', file.name)
+  await writeFile(pathname, buffer)
+  console.log(`open ${pathname} to see the uploaded file`)
 
-  return NextResponse.json({ success: true })
+  return NextResponse.json({ success: true, path: pathname })
 }
