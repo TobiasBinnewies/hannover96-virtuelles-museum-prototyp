62d51da3c89a4bd7c43042053d774e2a5cf652db
Loisployce <louis.dittmann@gmail.com>
2023-09-21 12:36:09 +0200

    Fixed user not being able to scroll through page when cursor in range of object canvas. Removed user to be able to zoom on the object

--- src/components/models/Model_Computer.js
+++ src/components/models/Model_Computer.js
@@ -10,13 +10,13 @@ function NewModel() {
 function ComponentModel() {
 
   return (
-        <div className={'w-[50%] h-screen bg-opacity-0 absolute right-[-10vw] z-20'}>
+        <div className={' absolute right-0 w-[20%] h-screen bg-opacity-0  z-20'}>
           <Canvas camera={{ fov: 20, zoom: 0.15, position:[2, 0, 0]}}>
             <ambientLight intensity={1.25} />
             <Suspense fallback={null}>
               <NewModel />
             </Suspense>
-            <OrbitControls minDistance={1} maxDistance={8}/>
+            <OrbitControls minDistance={1} maxDistance={8} enableZoom={false}/>
           </Canvas>
         </div>
   )
