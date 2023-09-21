a791d2483b8e6db19a15ac1ae6e52d1d3b61e46d
TobiasMBP <binnewiestobias@gmail.com>
2023-09-21 14:28:55 +0200

    Fixed path bug

--- src/app/api/secure/section-image/route.js
+++ src/app/api/secure/section-image/route.js
@@ -10,22 +10,21 @@ async function handler(req) {
   // const { userId } = req.auth
   // const { name, section, image } = await req.json()
 
-      const data = await req.formData()
-    const file = data.get('file')
+  const data = await req.formData()
+  const file = data.get('file')
 
-    if (!file) {
-      return NextResponse.json({ success: false })
-    }
+  if (!file) {
+    return NextResponse.json({ success: false })
+  }
 
-    const bytes = await file.arrayBuffer()
-    const buffer = Buffer.from(bytes)
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
 
-    // With the file data in the buffer, you can do whatever you want with it.
-    // For this, we'll just write it to the filesystem in a new location
-    const path = `./public/${file.name}`
-    await writeFile(path, buffer)
-    console.log(`open ${path} to see the uploaded file`)
+  // With the file data in the buffer, you can do whatever you want with it.
+  // For this, we'll just write it to the filesystem in a new location
+  const path = `public/${file.name}`
+  await writeFile(path, buffer)
+  console.log(`open ${path} to see the uploaded file`)
 
-    return NextResponse.json({ success: true })
-    
+  return NextResponse.json({ success: true })
 }
