ae049c55d39fe53ce148eeb6bced216d2f4b5a0d
Loisployce <louis.dittmann@gmail.com>
2023-09-28 13:05:30 +0200

    New Shoe Model

---
+++ src/components/models/Model_Shoe.js
@@ -0,0 +1,25 @@
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+
+function NewModel() {
+  const { scene } = useGLTF('/scanned_soccer_shoe.glb');
+  return <primitive position={[0, 0, 0]} object={scene}/>
+}
+
+function Model_Wd() {
+
+  return (
+        <div className={'absolute right-0 w-[20%] h-screen bg-opacity-0  z-20'}>
+          <Canvas camera={{ fov: 20, zoom: 0.33, position:[2, 0, 0]}}>
+            <ambientLight intensity={1.25} />
+            <Suspense fallback={null}>
+              <NewModel />
+            </Suspense>
+            <OrbitControls minDistance={2} maxDistance={8} enableZoom={false}/>
+          </Canvas>
+        </div>
+  )
+}
+
+export default Model_Wd
