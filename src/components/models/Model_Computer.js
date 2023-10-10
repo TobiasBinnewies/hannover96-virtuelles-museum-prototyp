291c6eb40a8de61e2e976480acf6f2dbf0052be7
Loisployce <louis.dittmann@gmail.com>
2023-10-10 18:26:08 +0200

    Model Anpassungen

--- src/components/models/Model_Computer.js
+++ src/components/models/Model_Computer.js
@@ -2,8 +2,22 @@ import { Stats, OrbitControls, Circle } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-export default function Model_Computer() {
+function Computer() {
   const gltf = useLoader(GLTFLoader, '/computer.glb')
+
+  return (
+    <primitive
+      object={gltf.scene}
+      /* Zweiter Wert gibt die Höhe des Objektes an der Vertikalen an*/
+      position={[0, 0.7, 0]}
+      /* Zweiter Wert gibt die Drehung des Objektes an der Vertikalen an*/
+      rotation={[0, 0, 0]}
+      children-0-castShadow
+    />
+  )
+}
+
+export default function Model_Computer() {
 
   return (
     <div className={'absolute right-0 w-[20%] h-screen bg-opacity-0  z-20'}>
@@ -11,14 +25,7 @@ export default function Model_Computer() {
         {/* AmbientLight und Spotlight notwendig, damit bei allen Objekten die Farben zu sehen sind. Ggf. je Objekt anpassen */}
         <ambientLight intensity={1} />
         <spotLight intensity={1} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-        <primitive
-          object={gltf.scene}
-          /* Zweiter Wert gibt die Höhe des Objektes an der Vertikalen an*/
-          position={[0, 0.7, 0]}
-          /* Zweiter Wert gibt die Drehung des Objektes an der Vertikalen an*/
-          rotation={[0, 0, 0]}
-          children-0-castShadow
-        />
+        <Computer/>
         {/* Mit der Min- und Max-Distance kann der Zoom an das Objekt eingestellt werden. Kleine Werte verringern die Distanz / Erhöhen den Zoom */}
         <OrbitControls target={[0, 1, 0]} minDistance={2} maxDistance={2} enableZoom={false} />
         {/* Einkommentieren, um die Achsen anzuzeigen: <axesHelper args={[5]} />*/}
