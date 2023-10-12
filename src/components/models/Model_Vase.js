1c8ca0072b8a6e2c5031ef463d8861bacc99b4ed
Loisployce <louis.dittmann@gmail.com>
2023-10-12 12:20:20 +0200

    Fix Model Relative to Section and added empty Model when model property = null

--- src/components/models/Model_Vase.js
+++ src/components/models/Model_Vase.js
@@ -9,7 +9,7 @@ export default function Model_Vase() {
   }
 
   return (
-    <div className={'absolute right-0 w-[20%] h-screen bg-opacity-0  z-20'}>
+    <div className={'relative right-0 w-[20%] h-screen bg-opacity-0  z-20'}>
       <iframe src="https://vr.scanblue.cloud/yx0v8o0acssv3snb2xgdltelx69zsp1u"
               style={myStyle} frameBorder="0" allowvr=""
               allowFullScreen="" mozallowfullscreen="true" webkitallowfullscreen="true" scrolling="no"></iframe>
--- src/components/models/models.js
+++ src/components/models/models.js
@@ -3,13 +3,15 @@ import Model_Computer from '@components/models/Model_Computer'
 import Model_Testobjekt from '@components/models/Model_Testobjekt'
 import Model_Ball from '@components/models/Model_Ball'
 import Model_Vase from '@components/models/Model_Vase'
+import Model_Empty from '@components/models/Model_Empty'
 
 const models = {
   Model_Wd: <Model_Wd/>,
   Model_Computer: <Model_Computer/>,
   Model_Testobjekt: <Model_Testobjekt/>,
   Model_Ball: <Model_Ball/>,
-  Model_Vase: <Model_Vase/>
+  Model_Vase: <Model_Vase/>,
+  Model_Empty: <Model_Empty/>
 }
 
 export default models
