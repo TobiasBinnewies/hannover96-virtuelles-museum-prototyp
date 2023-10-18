bd53165c635932c6c5817a00bff4374173843209
TobiasMBP <binnewiestobias@gmail.com>
2023-10-18 19:04:33 +0200

    Fixed key bug

--- src/components/section/SectionList.js
+++ src/components/section/SectionList.js
@@ -4,22 +4,19 @@ import { useSession } from '@/services/frontend/session'
 import models from '@components/models/models'
 import { Suspense } from 'react'
 
-export default async function SectionList({images, session}) {
+export default async function SectionList({ images, session }) {
   // const { session } = useSession({ redirect: false })
-  const sections = content.sections.map((item, index) => {
-    return (
-      <Suspense fallback={<p>Loading Sections...</p>}>
-          <Section
-            session={session}
-            key={index}
-            obj={item}
-            model={item.model == null ? models.Model_Empty : item.model}
-            arlink={item.arlink}
-            images={images}
-          />
-        </Suspense>
-    )
-  })
+  const sections = content.sections.map((item, index) => (
+    <Suspense key={index} fallback={<p>Loading Sections...</p>}>
+      <Section
+        session={session}
+        obj={item}
+        model={item.model == null ? models.Model_Empty : item.model}
+        arlink={item.arlink}
+        images={images}
+      />
+    </Suspense>
+  ))
 
   return (
     <div id="section-list" className="y-mandatory-scroll-snapping">
