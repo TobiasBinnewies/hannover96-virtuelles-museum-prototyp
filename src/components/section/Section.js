a8b56a62e100cb6925782328be3d9c883bd36805
TobiasMBP <binnewiestobias@gmail.com>
2024-02-29 12:24:21 +0100

    Fixed merge error

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -15,14 +15,14 @@ export default function Section({
   arlink,
   images,
 }) {
-  const mediaQuery = window.matchMedia("(min-width: 640px)")
-  const [titleSize, setTitleSize] = useState("4xl")
+  const mediaQuery = window.matchMedia('(min-width: 640px)')
+  const [titleSize, setTitleSize] = useState('4xl')
 
   useEffect(() => {
-    if(mediaQuery.matches) {
-      setTitleSize("6xl")
-    }else{
-      setTitleSize("2xl")
+    if (mediaQuery.matches) {
+      setTitleSize('6xl')
+    } else {
+      setTitleSize('2xl')
     }
   })
 
@@ -31,19 +31,27 @@ export default function Section({
       id={section.date.slice(-4)}
       className={'bg-primary-bg h-screen md:flex items-center childElement'}
     >
-      <div className={'w-[70%] md:w-[70%] md:left-64 relative md:flex top-36 md:top-24 z-30 ml-auto mr-10 md:mr-auto'}>
+      <div
+        className={
+          'w-[70%] md:w-[70%] md:left-64 relative md:flex top-36 md:top-24 z-30 ml-auto mr-10 md:mr-auto'
+        }
+      >
         <div>
-        <SectionSubtitle text={section.date} />
-        <SectionTitle title={section.title} size={titleSize} padding="6px" />
-        <SectionText text={section.description || section.content} />
-        <Suspense fallback={<p>Loading Modal...</p>}>
-          <SectionModal session={session} obj={section} images={images} />
-        </Suspense>
-        {/* <Suspense fallback={<p>Loading ARModel...</p>}>
-          {arlink != null ? <AR_Modal arlink={arlink} /> : <div />}
-        </Suspense>
+          <SectionSubtitle text={section.date} />
+          <SectionTitle title={section.title} size={titleSize} padding="6px" />
+          <SectionText text={section.description || section.content} />
+          <Suspense fallback={<p>Loading Modal...</p>}>
+            <SectionModal session={session} obj={section} images={images} />
+          </Suspense>
+          <Suspense fallback={<p>Loading ARModel...</p>}>
+            {arlink != null ? <AR_Modal arlink={arlink} /> : <div />}
+          </Suspense>
         </div>
-        <div className={"flex right-0 items-center justify-center md:justify-normal h-[25vh] md:w-[30%]"}>
+        <div
+          className={
+            'flex right-0 items-center justify-center md:justify-normal h-[25vh] md:w-[30%]'
+          }
+        >
           <Suspense fallback={<p>Loading ARModel...</p>}>{model}</Suspense>
         </div>
       </div>
