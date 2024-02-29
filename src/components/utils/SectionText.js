47ae8bf427e9168bb87cd7697e4d9122ca691a3c
TobiasMBP <binnewiestobias@gmail.com>
2024-02-29 13:08:35 +0100

    Line clamp

--- src/components/utils/SectionText.js
+++ src/components/utils/SectionText.js
@@ -1,7 +1,11 @@
 export default function SectionText(props) {
   return (
     <div className={'text-center pb-3'}>
-      <text className={'text-primary-text text-base font-sans font-normal'}>
+      <text
+        className={
+          'text-primary-text text-base font-sans font-normal line-clamp-3'
+        }
+      >
         {props.text}
       </text>
     </div>
