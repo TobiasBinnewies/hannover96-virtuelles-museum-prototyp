c8f6a523f11851e26581d1b4dd8f8bcdfadb685b
TobiasMBP <binnewiestobias@gmail.com>
2024-03-01 14:48:54 +0100

    Setup project for vercel postgres, renamed local envs

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ until nc -z db 5432; do
   sleep 1
 done
 
-DATABASE_URL=$DATABASE_URL npx prisma db push
+DATABASE_PRISMA_URL=$DATABASE_URL POSTGRES_URL_NON_POOLING=$DATABASE_URL npx prisma db push
 
 psql $DATABASE_URL -f db/init.sql || true
 
