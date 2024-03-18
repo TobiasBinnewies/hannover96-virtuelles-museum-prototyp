9cf625c22be4302ea69f7974f502eadf3a4648fc
TobiasMBP <binnewiestobias@gmail.com>
2024-03-18 12:30:32 +0100

    Updated Homepage, Added Link in Navbar

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -4,35 +4,35 @@ import Kachel from './Kachel'
 const kacheln = [
   {
     title: 'Hannover 96 - Home',
-    image: '/H96_Header_Logo.png',
+    image: '/Stadion96.jpeg',
     url: 'https://www.hannover96.de/startseite',
   },
 
   {
     title: 'Virtuelles Museum',
-    image: '/H96_Header_Logo.png',
-    url: '/timeline-start',
+    image: '/Archiv96.jpg',
+    url: '/timeline',
   },
 
   {
     title: 'Stamme 96 - Matterport',
-    image: '/H96_Header_Logo.png',
+    image: '/Stamme96.jpg',
     url: 'https://my.mpskin.com/de/tour/rh179agyah',
   },
 ]
 
 function Homepage() {
   return (
-    <div className={'h-screen p-10 flex items-center'}>
-        <div
-          className={
-            'h-[50%] w-[50%] m-auto flex flex-col lg:flex-row items-center container'
-          }
-        >
-          {kacheln.map((kachel, index) => (
-            <Kachel key={index} {...kachel} />
-          ))}
-        </div>
+    <div className={'h-screen bg-primary-bg p-10 flex items-center'}>
+      <div
+        className={
+          'h-[50%] w-[50%] m-auto flex flex-col lg:flex-row items-center container'
+        }
+      >
+        {kacheln.map((kachel, index) => (
+          <Kachel key={index} {...kachel} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/homepage/Kachel.js
+++ src/components/homepage/Kachel.js
@@ -1,17 +1,16 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Kachel({ title, image, url }) {
   return (
     <Link href={url}>
       <div
         className={
-          'lg:w-[12vw] w-[50vw] lg:h-[70vh] h-[20vh] bg-cta-bg mx-5 my-1 lg:-skew-x-12 lg:skew-y-0 -skew-y-6 overflow-hidden border-4 relative'
+          'lg:w-[15vw] w-[50vw] lg:h-[70vh] h-[20vh] bg-cta-bg mx-5 my-1 lg:-skew-x-12 lg:skew-y-0 -skew-y-6 overflow-hidden border-4 relative border-black hover:scale-105 transition-all'
         }
         style={{ minWidth: '100px' }}
       >
         <div
-          className="h-full lg:w-[25vw] lg:-translate-x-20 lg:skew-x-12 lg:-skew-y-0 skew-y-6 bg-white lg:scale-100 scale-125 text-center w-full bg-no-repeat bg-center bg-cover absolute"
+          className="h-[calc(100%*1.1)] lg:w-[calc(100%*2)] lg:-translate-x-[25%] lg:skew-x-12 lg:-skew-y-0 skew-y-6 bg-white lg:scale-100 scale-125 text-center w-full bg-no-repeat bg-center bg-cover absolute"
           style={{ backgroundImage: `url(${image})` }}
         ></div>
         <div className="lg:skew-x-12 lg:-skew-y-0 skew-y-6 absolute w-full justify-center flex left-0 lg:bottom-20 scale-0 lg:scale-100">
@@ -27,8 +26,8 @@ export default function Kachel({ title, image, url }) {
 
 function Title({title}) {
     return (
-        <div className="w-3/4 min-h-min p-3 rounded bg-blue-500 text-center">
-          {title}
-        </div>
+      <div className="bg-white bg-opacity-80 text-black rounded-2xl text-center p-4 mt-2 ml-auto mr-auto w-3/4 font-bold">
+        {title}
+      </div>
     )
 }
